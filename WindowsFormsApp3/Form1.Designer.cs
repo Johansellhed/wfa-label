241444446cbda9f28218143a7c09ed64531e3f1b
Johansellhed <112383566+Johansellhed@users.noreply.github.com>
2023-03-01 11:02:03 +0100

    ...

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(290, 279);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Klicka här för att ändra min bakgrundsfärg";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.DoubleClick += new System.EventHandler(this.label1_DoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(294, 348);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(196, 53);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Klicka här för att ändra formens backgrund färg";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(290, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(196, 62);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Klicka är";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(124, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 50);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            this.label4.MouseEnter += new System.EventHandler(this.label4_MouseEnter);
+            this.label4.MouseLeave += new System.EventHandler(this.label4_MouseLeave);
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(298, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(231, 50);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(200, 109);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 23);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            this.label6.MouseEnter += new System.EventHandler(this.label6_MouseEnter);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(559, 33);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(80, 20);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "linkLabel1";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int num1 = 0;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+            label1.BackColor = Color.Magenta;
+            label1.ForeColor = Color.White;
+            label2.ForeColor = Color.White;
+            label3.ForeColor = Color.White;
+
+
+
+        }
+
+        private void label1_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Magenta;
+            
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            num1++;
+            this.Text = num1.ToString();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_MouseEnter(object sender, EventArgs e)
+        {
+            string x = "Tack för att du besökte oss!";
+
+            label5.Text = x;
+        }
+
+        private void label4_MouseLeave(object sender, EventArgs e)
+        {
+            label5.Text = "label5";
+        }
+
+        private void label6_MouseEnter(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            int num2 = random.Next(1, 100);
+            int num3 = random.Next(1, 100);
+
+            label6.Location = new Point(num2, num3);
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabel1.LinkVisited = true;
+
+
+            System.Diagnostics.Process.Start("http://www.microsoft.com");
+        }
+    }
+}
